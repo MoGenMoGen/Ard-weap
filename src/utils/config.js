c3f1986f216ef2ff4c4cf798c3bb138c2fc79cc0
kdw <1197929159@qq.com>
2021-12-20 23:53:23 +0800

    小修改

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
-// const serverURL = 'https://order.ilikeit-runner.com'
-const serverURL = 'http://u2768442w0.qicp.vip'
+const serverURL = 'https://order.ilikeit-runner.com'
+// const serverURL = 'http://u2768442w0.qicp.vip'
 // const serverURL = 'http://lwkacgr.nat.ipyingshe.com'
 // const serverURL = "http://192.168.2.130";
 const imgURL = "https://h5.guluint.com/ruicao/Image/";
